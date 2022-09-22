494c1dac0c649a4d3cf621e9717962bf4f536926
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-09-22 21:36:54 +0300

    start project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Loader } from "./Loader";
+import './styles.css'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>React homework template</h1>
+      <button className="Button">Это кнопка </button>
+
+      <Loader/>
+    </div>
+  );
+};
