080b67b030e54af6a5c7b9e3bb379704d2d6c46f
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-09-23 22:24:40 +0300

    up

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -7,15 +7,23 @@ export class ImageGallery extends Component {
   state = {
     images: null,
     loading: false,
-    error: '',
+    error: null,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps, prevStats) {
     if (prevProps.searchName !== this.props.searchName) {
       this.setState({ loading: true });
       const { searchName } = this.props;
-      api
-        .FetchRequest(searchName)
+      fetch(
+        `https://pixabay.com/api/?q=${searchName}&page=1&key=29146874-e25e04f0bbd5e8c4fffc4a4f6&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error(`We don't have ${searchName}...`));
+        })
         .then(data => {
           const images = data.hits;
           this.setState({ images });
@@ -27,11 +35,38 @@ export class ImageGallery extends Component {
   }
 
   render() {
-    const images = this.state.images;
+    const { images, error, loading } = this.state;
+    // if (this.state.status === 'idle') {
+    //   return <h2>Enter a keyword to search!</h2>;
+    // }
+
+    // if (this.state.status === 'pending') {
+    //   return <Loader />;
+    // }
+
+    // if (this.state.status === 'rejected') {
+    //   return <h2>{`We don't have this...`}</h2>;
+    // }
+
+    // if (this.state.status === 'resolved') {
+    //   return (
+    //     <ul className="ImageGallery">
+    //       {images.map(item => (
+    //         <ImageGallaryItem
+    //           key={item.id}
+    //           imgURL={item.webformatURL}
+    //           imgTitle={item.tags}
+    //         />
+    //       ))}
+    //     </ul>
+    //   );
+    // }
+
     return (
       <div className="container">
-        {images === [] && <h2>No matches</h2>}
-        {this.state.loading && <Loader />}
+        {/* {error && <h2>{error.message}</h2>} */}
+        {images === [] && <h2>{`We don't have this...`}</h2>}
+        {loading && <Loader />}
         {!this.props.searchName && <h2>Enter a keyword to search!</h2>}
         {images && (
           <ul className="ImageGallery">
